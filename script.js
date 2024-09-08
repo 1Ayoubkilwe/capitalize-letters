eeed145f6206b411aeaa130da864f6520a9dbe8a
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 16:35:34 +0300

    answers

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function makeCapital(str) {
+    return str
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+  
+  
+  console.log(makeCapital("my name is ali")); // "My Name Is Ali"
+  console.log(makeCapital("fikrcamp Bootcamp")); // "Fikrcamp Bootcamp"
+  console.log(makeCapital("i live In Paris")); // "I Live In Paris"
+  
